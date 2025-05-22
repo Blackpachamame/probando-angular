67c37ff19efb8bfc7410d1b9edbb651ad5b70c00
Marcos Travaglini <marcos.travaglini@est.fi.uncoma.edu.ar>
2025-05-22 13:09:17 -0300

    Gestionando el ESTADO de una aplicación en Angular con BehaviorSubject y RxJS

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,18 +1,27 @@
-import { Component, inject } from '@angular/core';
+import { Component, DestroyRef, inject } from '@angular/core';
 import { HeroListComponent } from '../../components/hero-list/hero-list.component';
 import { HeroService } from '../../shared/services/hero.service';
 import { AsyncPipe } from '@angular/common';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-home',
   imports: [HeroListComponent, AsyncPipe],
   template: `
-    @let heroes = (heroes$ | async)?.heroes; @if(heroes){
+    @let heroes = heroes$ | async; @if(heroes){
     <app-hero-list [heroes]="heroes" />
     }
   `,
 })
 export class HomeComponent {
   readonly #heroService = inject(HeroService);
-  heroes$ = this.#heroService.load();
+  readonly #destroyRef = inject(DestroyRef);
+  heroes$ = this.#heroService.heroes$;
+
+  constructor() {
+    this.#heroService
+      .load()
+      .pipe(takeUntilDestroyed(this.#destroyRef))
+      .subscribe();
+  }
 }
--- src/app/shared/services/hero.service.ts
+++ src/app/shared/services/hero.service.ts
@@ -2,18 +2,28 @@ import { Hero, PowerStats } from '../interfaces/hero.interface';
 import { inject, Injectable } from '@angular/core';
 import { HeroServiceAbstract } from './hero.service.abstract';
 import { HttpClient } from '@angular/common/http';
-import { catchError, Observable, of, throwError } from 'rxjs';
+import {
+  BehaviorSubject,
+  catchError,
+  Observable,
+  of,
+  tap,
+  throwError,
+} from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HeroService extends HeroServiceAbstract {
+  readonly #heroesSubject = new BehaviorSubject<Hero[]>([]);
+  readonly heroes$ = this.#heroesSubject.asObservable(); /* Receptor */
   readonly #httpClient = inject(HttpClient);
 
   load(): Observable<{ heroes: Hero[]; total: number }> {
     return this.#httpClient
       .get<{ heroes: Hero[]; total: number }>(this.API_ENDPOINT)
       .pipe(
+        tap((result) => this.#heroesSubject.next(result.heroes)),
         catchError((error) => {
           console.error('Failed to load heroes', error);
           return throwError(() => error);
@@ -21,8 +31,12 @@ export class HeroService extends HeroServiceAbstract {
       );
   }
 
-  add(hero: Partial<Hero>): Observable<Hero> {
+  add(hero: Hero): Observable<Hero> {
     return this.#httpClient.post<Hero>(this.API_ENDPOINT, hero).pipe(
+      tap((newHero) => {
+        const currentHeroes = this.#heroesSubject.getValue();
+        this.#heroesSubject.next([...currentHeroes, newHero]);
+      }),
       catchError((error) => {
         console.error('Failed to add an hero', error);
         return throwError(() => error);
@@ -47,6 +61,13 @@ export class HeroService extends HeroServiceAbstract {
     return this.#httpClient
       .put<Hero>(`${this.API_ENDPOINT}/${heroToUpdate.id}`, heroToUpdate)
       .pipe(
+        tap((updatedHero) => {
+          const currentHeroes = this.#heroesSubject.getValue();
+          const updatedHeroes = currentHeroes.map((hero) =>
+            hero.id === updatedHero.id ? updatedHero : hero
+          );
+          this.#heroesSubject.next(updatedHeroes);
+        }),
         catchError((error) => {
           console.error('Failed to update hero', error);
           return throwError(() => error);
@@ -54,9 +75,16 @@ export class HeroService extends HeroServiceAbstract {
       );
   }
   remove(hero: Hero): Observable<void> {
+    const { id } = hero;
     return this.#httpClient
       .delete<void>(`${this.API_ENDPOINT}/${hero.id}`)
       .pipe(
+        tap(() => {
+          const updateState = this.#heroesSubject
+            .getValue()
+            .filter((hero) => hero.id !== id);
+          this.#heroesSubject.next(updateState);
+        }),
         catchError((error) => {
           console.error('Error deleting hero', error);
           return throwError(() => error);
@@ -67,6 +95,7 @@ export class HeroService extends HeroServiceAbstract {
     return this.#httpClient
       .get<{ heroes: Hero[]; total: number }>(this.API_ENDPOINT)
       .pipe(
+        tap((result) => this.#heroesSubject.next(result.heroes)),
         catchError((error) => {
           console.error('Error deleting hero', error);
           return throwError(() => error);
